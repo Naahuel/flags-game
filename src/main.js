910b891f6d2219337c5689cbb94dcb0784812e6a
Nahuel José <nahueljose@gmail.com>
2018-11-06 20:45:27 -0300

    Vue material components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
+import {
+  MdContent,
+  MdToolbar,
+} from 'vue-material/dist/components';
 import App from './App.vue';
 import store from './store';
+
 import './registerServiceWorker';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdContent);
+Vue.use(MdToolbar);
 
 Vue.config.productionTip = false;
 
