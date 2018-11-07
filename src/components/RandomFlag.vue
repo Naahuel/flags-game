53994f7b4a3f4f15735726009f9461b8f848e976
Nahuel José <nahueljose@gmail.com>
2018-11-06 22:07:46 -0300

    Demo component for random flag

---
+++ src/components/RandomFlag.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <button @click="getRandomFlag">Random flag</button>
+    <h1>{{flags[randomFlagIndex].name}}</h1>
+    <p>
+      <img :src="getIconPath(flags[randomFlagIndex].code)" alt="" width="200">
+    </p>
+  </div>
+</template>
+
+<script>
+// State utils
+import { mapState } from 'vuex';
+
+export default {
+  // Component name
+  name: 'RandomFlag',
+
+  // Imported components
+  components: {
+
+  },
+
+  // Data
+  data() {
+    return {
+      randomFlagIndex: 0
+    }
+  },
+
+  // Methods
+  methods: {
+    getIconPath (iconName) {
+        return iconName ? require(`../assets/flags-normal/${iconName}.png`) : '';
+    },
+    getRandomFlag () {
+      this.randomFlagIndex = Math.floor(Math.random() * (this.flags.length - 1));
+    }
+  },
+
+  // Computed methods & state
+  computed: {
+    ...mapState({
+      flags: ({flags}) => flags,
+    }),
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
